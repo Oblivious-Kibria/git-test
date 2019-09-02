15d2b8d3f9ff0ad5ca4170890f3288859494dada
Oblivious Kibria <g.kibria78@gmail.com>
2019-09-02 17:46:11 +0600

    min function is addedd

--- app/src/main/java/com/cerebrum/gittest/MainActivity.java
+++ app/src/main/java/com/cerebrum/gittest/MainActivity.java
@@ -9,11 +9,18 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private void findMin(){
+          // Find minimum value;
+    }
+
 
 
 }
